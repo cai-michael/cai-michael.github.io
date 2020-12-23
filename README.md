a92794af5d5abd823b1cb1d84d20f29ce17066be
cai-michael <cai.michael.r@gmail.com>
2020-12-22 17:50:08 -0700

    Hamburger Menu Implemented

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cai-michael.github.io
+
+A simple static site created in React to showcase projects and my skills as a developer
--- website/src/App.js
+++ website/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./global";
+import { theme } from "./theme";
+import { Burger, Menu } from "./components";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useOnClickOutside } from './hooks';
+import logo from "./images/raccoon-image-1.png";
+import "./App.css";
+
 
 function App() {
+  const node = useRef();
+  const [open, setOpen] = useState(false);
+  useOnClickOutside(node, () => setOpen(false));
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+
+      <Router>
+        <div ref={node}>
+          <Burger open={open} setOpen={setOpen}/>
+          <Menu open={open} setOpen={setOpen}/>
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, I'm Michael Cai :)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
+}
+
+function About() {
+  return <h1>About Or Something</h1>;
+}
+
+function Users() {
+  return <h2>Woooaaaahhhhhh!!!</h2>;
+}
+
+function showHamburger() {
+  var x = document.getElementById("hamburgerOptions");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
 
 export default App;
