13ac24b6663e10da3938cf36287cdc1a045b7b31
Michael Cai <31701749+cai-michael@users.noreply.github.com>
2020-12-24 12:34:28 -0700

    Fix Grammar Thanks Julia

--- website/src/App.js
+++ website/src/App.js
@@ -46,14 +46,14 @@ function Home() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="A spinning raccoon" />
         <h1>Hi, I'm Michael Cai :)</h1>
-        <p>I'm still working on this trying to learn React and Github Workflows/Pages! Come back later!</p>
+        <p>I'm still working on this. I am trying to learn React and Github Workflows/Pages! Come back later!</p>
       </header>
     </div>
   );
 }
 
 function Resume() {
-  return <h1>I have an diploma I swear</h1>;
+  return <h1>I have a diploma. I swear</h1>;
 }
 
 function Contact() {
