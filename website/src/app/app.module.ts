be071f835fbd66132196836a1e232618fd8fedd1
cai-michael <cai.michael.r@gmail.com>
2023-05-21 17:48:55 -0700

    Added HashLocationStrategy

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ResumeComponent } from './resume/resume.component';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SummaryComponent } from './summary/summary.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -15,9 +16,18 @@ import { SummaryComponent } from './summary/summary.component';
     HomeComponent,
     SummaryComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule
+  ],
+  providers: [
+    {
+      provide: LocationStrategy, 
+      useClass: HashLocationStrategy
+    }
+  ],
   bootstrap: [AppComponent],
   exports: [RouterModule],
+  
 })
 export class AppModule {}
