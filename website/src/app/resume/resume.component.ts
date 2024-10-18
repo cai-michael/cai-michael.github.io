55f2f1bd5d53ad94d3891c8c042192b287d39f99
Michael Cai <31701749+cai-michael@users.noreply.github.com>
2024-10-18 00:14:50 -0700

    Feature: Resume as PDF and Updated Content/Styling (#20)

--- website/src/app/resume/resume.component.ts
+++ website/src/app/resume/resume.component.ts
@@ -5,14 +5,30 @@ import { Component } from '@angular/core';
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.css'],
 })
-
 export class ResumeComponent {
   currentDateString(startDate: String) {
-    const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    const month = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
     var currentDate = new Date();
     var currentMonth = month.at(currentDate.getMonth());
     var currentYear = currentDate.getFullYear();
-    var dates = `${startDate} - ${currentMonth} ${currentYear} (Present)`
+    var dates = `${startDate} - ${currentMonth} ${currentYear} (Present)`;
     return dates;
+  }
+
+  downloadResume() {
+    window.open('/assets/michael-cai-resume.pdf', '_blank');
   }
 }
