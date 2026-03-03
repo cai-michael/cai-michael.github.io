97be619f12d79e4e51ebcc968fc1122c7435419d
Michael Cai <31701749+cai-michael@users.noreply.github.com>
2026-03-02 17:43:46 -0700

    Security Fixes + Node 25 (#22)

--- website/src/app/app.component.spec.ts
+++ website/src/app/app.component.spec.ts
@@ -1,12 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { ResumeComponent } from './resume/resume.component';
+import { RouterModule } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterModule.forRoot([]), ],
       declarations: [AppComponent],
     })
   );
